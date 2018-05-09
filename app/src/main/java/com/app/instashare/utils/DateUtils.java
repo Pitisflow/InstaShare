2bbb62346f0405366657f986b19ca0ffed6f9666
Javier García Antúnez <javiergarciantunez@gmail.com>
2018-05-09 12:21:04 +0200

    improved DateUtils.getPostDateFromTimestamp

--- app/src/main/java/com/app/instashare/utils/DateUtils.java
+++ app/src/main/java/com/app/instashare/utils/DateUtils.java
@@ -2,6 +2,13 @@ package com.app.instashare.utils;
 
 import android.content.Context;
 
+import com.app.instashare.R;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Created by Pitisflow on 4/5/18.
  */
@@ -20,10 +27,24 @@ public class DateUtils {
         long currentTimestamp = System.currentTimeMillis();
         long difference = currentTimestamp - postTimestamp;
 
-        if (difference < MINUTE) return "Hace un momento";
-        else if ((difference >= MINUTE) && (difference < HOUR)) return "Hace " + difference / MINUTE + " minutos";
-        else if ((difference >= HOUR) && (difference < DAY)) return "Hace " + difference / HOUR + " horas";
-        else if ((difference >= DAY) && (difference < DAY * 5)) return "Hace " + difference / DAY + " dias";
-        else return "mucho";
+        Date date = new Date(postTimestamp);
+        DateFormat format = new SimpleDateFormat("dd MMMM yyyy', 'HH:mm", Locale.getDefault());
+
+
+
+        if (difference < MINUTE) return context.getString(R.string.dateutils_post_while_ago);
+        else if ((difference >= MINUTE) && (difference < HOUR)) {
+            if (difference >= MINUTE && difference <= MINUTE * 2) return context.getString(R.string.dateutils_post_minute_ago);
+            else return context.getString(R.string.dateutils_post_minutes_ago, difference / MINUTE);
+        }
+        else if ((difference >= HOUR) && (difference < DAY)) {
+            if (difference >= HOUR && difference <= HOUR * 2) return context.getString(R.string.dateutils_post_hour_ago);
+            else return context.getString(R.string.dateutils_post_hours_ago, difference / HOUR);
+        }
+        else if ((difference >= DAY) && (difference < DAY * 6)) {
+            if (difference >= DAY && difference <= DAY * 2) return context.getString(R.string.dateutils_post_day_ago);
+            else return context.getString(R.string.dateutils_post_days_ago, difference / DAY);
+        }
+        else return format.format(date);
     }
 }
